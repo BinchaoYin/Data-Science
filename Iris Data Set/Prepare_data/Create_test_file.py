8252c2f4f77b4b676fb222d92cfe8cd6e71a6ea9
DredYin <yin.binchao@irdeto.com>
2019-06-26 15:57:55 +0200

    Create training file(2/3) and test file(1/3)

--- Iris Data Set/Prepare_data/Create_test_file.py
+++ Iris Data Set/Prepare_data/Create_test_file.py
@@ -1,15 +1,15 @@
 import random
 import os
 # clean up
-#if os.path.exists("training_iris.data"):
-#    os.remove("training_iris.data")
+#if os.path.exists("Prepare_data/training_iris.data"):
+#    os.remove("Prepare_data/training_iris.data")
 #if os.path.exists("test_iris.data"):
 #    os.remove("test_iris.data")
 # open source
 original_file = open("Raw_data/iris.data", "r")
 # create output (w+, create new one anyway)
-training_file = open("training_iris.data", "w+")
-test_file = open("test_iris.data", "w+")
+training_file = open("Prepare_data/training_iris.data", "w+")
+test_file = open("Prepare_data/test_iris.data", "w+")
 # select: 1/3 - test ; 2/3 - training
 for i in original_file:
     seed = random.randint(1, 100000000000)
